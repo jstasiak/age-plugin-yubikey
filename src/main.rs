262883a50c0298ac3cc6a3dc4158e2ecdd49ed7b
Jack Grigg <thestr4d@gmail.com>
2021-01-01 03:23:37 +0000

    Implement --list and --list-all commands

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,44 @@
 use age_plugin::run_state_machine;
 use gumdrop::Options;
+use yubikey_piv::{
+    certificate::PublicKeyInfo,
+    key::{RetiredSlotId, SlotId},
+    policy::{PinPolicy, TouchPolicy},
+    Key, Readers,
+};
 
 mod error;
+mod p256;
 mod plugin;
+mod util;
 
 use error::Error;
+
+const PLUGIN_NAME: &str = "age-plugin-yubikey";
+const RECIPIENT_PREFIX: &str = "age1yubikey";
+
+const USABLE_SLOTS: [RetiredSlotId; 20] = [
+    RetiredSlotId::R1,
+    RetiredSlotId::R2,
+    RetiredSlotId::R3,
+    RetiredSlotId::R4,
+    RetiredSlotId::R5,
+    RetiredSlotId::R6,
+    RetiredSlotId::R7,
+    RetiredSlotId::R8,
+    RetiredSlotId::R9,
+    RetiredSlotId::R10,
+    RetiredSlotId::R11,
+    RetiredSlotId::R12,
+    RetiredSlotId::R13,
+    RetiredSlotId::R14,
+    RetiredSlotId::R15,
+    RetiredSlotId::R16,
+    RetiredSlotId::R17,
+    RetiredSlotId::R18,
+    RetiredSlotId::R19,
+    RetiredSlotId::R20,
+];
 
 #[derive(Debug, Options)]
 struct PluginOptions {
@@ -31,6 +65,57 @@ struct PluginOptions {
     list_all: bool,
 }
 
+fn list(all: bool) -> Result<(), Error> {
+    let mut readers = Readers::open()?;
+
+    for reader in readers.iter()? {
+        let mut yubikey = reader.open()?;
+
+        for key in Key::list(&mut yubikey)? {
+            // We only use the retired slots.
+            let slot = match key.slot() {
+                SlotId::Retired(slot) => slot,
+                _ => continue,
+            };
+
+            // Only P-256 keys are compatible with us.
+            let recipient = match key.certificate().subject_pki() {
+                PublicKeyInfo::EcP256(pubkey) => match p256::Recipient::from_pubkey(*pubkey) {
+                    Some(recipient) => recipient,
+                    None => continue,
+                },
+                _ => continue,
+            };
+
+            let (_, cert) = x509_parser::parse_x509_der(key.certificate().as_ref()).unwrap();
+            let (name, pin_policy, touch_policy) =
+                match util::extract_name_and_policies(&mut yubikey, &key, &cert, all) {
+                    Some(res) => res,
+                    None => continue,
+                };
+
+            println!(
+                "#      Serial: {}, Slot: {}",
+                yubikey.serial(),
+                // Use 1-indexing in the UI for niceness
+                USABLE_SLOTS.iter().position(|s| s == &slot).unwrap() + 1,
+            );
+            println!("#        Name: {}", name);
+            println!("#     Created: {}", cert.validity().not_before.to_rfc2822());
+            println!("#  PIN policy: {}", util::pin_policy_to_str(pin_policy));
+            println!(
+                "# Touch policy: {}",
+                util::touch_policy_to_str(touch_policy)
+            );
+            println!("{}", recipient.to_string());
+            println!();
+        }
+        println!();
+    }
+
+    Ok(())
+}
+
 fn main() -> Result<(), Error> {
     let opts = PluginOptions::parse_args_default_or_exit();
 
@@ -55,9 +140,9 @@ fn main() -> Result<(), Error> {
     } else if opts.identity {
         todo!()
     } else if opts.list {
-        todo!()
+        list(false)
     } else if opts.list_all {
-        todo!()
+        list(true)
     } else {
         // TODO: CLI identity generation
         Ok(())
---
+++ src/util.rs
@@ -0,0 +1,108 @@
+use der_parser::oid::Oid;
+use x509_parser::X509Certificate;
+use yubikey_piv::{
+    policy::{PinPolicy, TouchPolicy},
+    Key, YubiKey,
+};
+
+use crate::PLUGIN_NAME;
+
+const POLICY_EXTENSION_OID: &[u64] = &[1, 3, 6, 1, 4, 1, 41482, 3, 8];
+
+pub(crate) fn pin_policy_to_str(policy: Option<PinPolicy>) -> &'static str {
+    match policy {
+        Some(PinPolicy::Always) => "Always (A PIN is required for every decryption, if set)",
+        Some(PinPolicy::Once) => "Once   (A PIN is required once per session, if set)",
+        Some(PinPolicy::Never) => "Never  (A PIN is NOT required to decrypt)",
+        _ => "Unknown",
+    }
+}
+
+pub(crate) fn touch_policy_to_str(policy: Option<TouchPolicy>) -> &'static str {
+    match policy {
+        Some(TouchPolicy::Always) => "Always (A physical touch is required for every decryption)",
+        Some(TouchPolicy::Cached) => {
+            "Cached (A physical touch is required for decryption, and is cached for 15 seconds)"
+        }
+        Some(TouchPolicy::Never) => "Never  (A physical touch is NOT required to decrypt)",
+        _ => "Unknown",
+    }
+}
+
+pub(crate) fn extract_name(cert: &X509Certificate, all: bool) -> Option<(String, bool)> {
+    // Look at Subject Organization to determine if we created this.
+    match cert.subject().iter_organization().next() {
+        Some(org) if org.as_str() == Ok(PLUGIN_NAME) => {
+            // We store the identity name as a Common Name attribute.
+            let name = cert
+                .subject()
+                .iter_common_name()
+                .next()
+                .and_then(|cn| cn.as_str().ok())
+                .map(|s| s.to_owned())
+                .unwrap_or_default(); // TODO: This should always be present.
+
+            Some((name, true))
+        }
+        _ => {
+            // Not one of ours, but we've already filtered for compatibility.
+            if !all {
+                return None;
+            }
+
+            // Display the entire subject.
+            let name = cert.subject().to_string();
+
+            Some((name, false))
+        }
+    }
+}
+
+pub(crate) fn extract_name_and_policies(
+    yubikey: &mut YubiKey,
+    key: &Key,
+    cert: &X509Certificate,
+    all: bool,
+) -> Option<(String, Option<PinPolicy>, Option<TouchPolicy>)> {
+    // We store the PIN and touch policies for identities in their certificates
+    // using the same certificate extension as PIV attestations.
+    // https://developers.yubico.com/PIV/Introduction/PIV_attestation.html
+    let policies = |c: &X509Certificate| {
+        let policy = c
+            .extensions()
+            .get(&Oid::from(POLICY_EXTENSION_OID).unwrap())
+            .unwrap();
+        let pin_policy = match policy.value[0] {
+            0x01 => PinPolicy::Never,
+            0x02 => PinPolicy::Once,
+            0x03 => PinPolicy::Always,
+            _ => unreachable!(),
+        };
+        let touch_policy = match policy.value[1] {
+            0x01 => TouchPolicy::Never,
+            0x02 => TouchPolicy::Always,
+            0x03 => TouchPolicy::Cached,
+            _ => unreachable!(),
+        };
+        (Some(pin_policy), Some(touch_policy))
+    };
+
+    extract_name(cert, all).map(|(name, ours)| {
+        if ours {
+            let (pin_policy, touch_policy) = policies(&cert);
+            (name, pin_policy, touch_policy)
+        } else {
+            // We can extract the PIN and touch policies via an attestation. This
+            // is slow, but the user has asked for all compatible keys, so...
+            let (pin_policy, touch_policy) = match yubikey_piv::key::attest(yubikey, key.slot()) {
+                Ok(buf) => {
+                    let (_, c) = x509_parser::parse_x509_der(&buf).unwrap();
+                    policies(&c)
+                }
+                Err(_) => (None, None),
+            };
+
+            (name, pin_policy, touch_policy)
+        }
+    })
+}
